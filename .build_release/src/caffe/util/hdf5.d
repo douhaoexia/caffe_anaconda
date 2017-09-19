4fe5d1bd2fdf57b2f13a7f52fdbf740984e5c59a
douhaoexia <douhaoexia@163.com>
2017-09-19 09:59:19 +0800

    new caffe for anaconda

---
+++ .build_release/src/caffe/util/hdf5.d
@@ -0,0 +1,208 @@
+.build_release/src/caffe/util/hdf5.o: src/caffe/util/hdf5.cpp \
+ include/caffe/util/hdf5.hpp /home/dou/anaconda3/include/hdf5.h \
+ /home/dou/anaconda3/include/H5public.h \
+ /home/dou/anaconda3/include/H5pubconf.h \
+ /home/dou/anaconda3/include/H5version.h \
+ /home/dou/anaconda3/include/H5api_adpt.h \
+ /home/dou/anaconda3/include/H5Apublic.h \
+ /home/dou/anaconda3/include/H5Ipublic.h \
+ /home/dou/anaconda3/include/H5Opublic.h \
+ /home/dou/anaconda3/include/H5Lpublic.h \
+ /home/dou/anaconda3/include/H5Tpublic.h \
+ /home/dou/anaconda3/include/H5ACpublic.h \
+ /home/dou/anaconda3/include/H5Cpublic.h \
+ /home/dou/anaconda3/include/H5Dpublic.h \
+ /home/dou/anaconda3/include/H5Epublic.h \
+ /home/dou/anaconda3/include/H5Epubgen.h \
+ /home/dou/anaconda3/include/H5Fpublic.h \
+ /home/dou/anaconda3/include/H5FDpublic.h \
+ /home/dou/anaconda3/include/H5Gpublic.h \
+ /home/dou/anaconda3/include/H5MMpublic.h \
+ /home/dou/anaconda3/include/H5Ppublic.h \
+ /home/dou/anaconda3/include/H5Zpublic.h \
+ /home/dou/anaconda3/include/H5PLpublic.h \
+ /home/dou/anaconda3/include/H5Rpublic.h \
+ /home/dou/anaconda3/include/H5Spublic.h \
+ /home/dou/anaconda3/include/H5FDcore.h \
+ /home/dou/anaconda3/include/H5FDfamily.h \
+ /home/dou/anaconda3/include/H5FDlog.h \
+ /home/dou/anaconda3/include/H5FDmpi.h \
+ /home/dou/anaconda3/include/H5FDmpio.h \
+ /home/dou/anaconda3/include/H5FDmulti.h \
+ /home/dou/anaconda3/include/H5FDsec2.h \
+ /home/dou/anaconda3/include/H5FDstdio.h \
+ /home/dou/anaconda3/include/H5FDdirect.h \
+ /home/dou/anaconda3/include/hdf5_hl.h /home/dou/anaconda3/include/hdf5.h \
+ /home/dou/anaconda3/include/H5DOpublic.h \
+ /home/dou/anaconda3/include/H5DSpublic.h \
+ /home/dou/anaconda3/include/H5LTpublic.h \
+ /home/dou/anaconda3/include/H5IMpublic.h \
+ /home/dou/anaconda3/include/H5TBpublic.h \
+ /home/dou/anaconda3/include/H5PTpublic.h include/caffe/blob.hpp \
+ include/caffe/common.hpp include/caffe/util/device_alternate.hpp \
+ /usr/local/cuda/include/cublas_v2.h /usr/local/cuda/include/cublas_api.h \
+ /usr/local/cuda/include/driver_types.h \
+ /usr/local/cuda/include/host_defines.h \
+ /usr/local/cuda/include/cuComplex.h \
+ /usr/local/cuda/include/vector_types.h \
+ /usr/local/cuda/include/builtin_types.h \
+ /usr/local/cuda/include/device_types.h \
+ /usr/local/cuda/include/surface_types.h \
+ /usr/local/cuda/include/texture_types.h \
+ /usr/local/cuda/include/cuda_fp16.h \
+ /usr/local/cuda/include/library_types.h /usr/local/cuda/include/cuda.h \
+ /usr/local/cuda/include/cuda_runtime.h \
+ /usr/local/cuda/include/host_config.h \
+ /usr/local/cuda/include/channel_descriptor.h \
+ /usr/local/cuda/include/cuda_runtime_api.h \
+ /usr/local/cuda/include/cuda_device_runtime_api.h \
+ /usr/local/cuda/include/driver_functions.h \
+ /usr/local/cuda/include/vector_functions.h \
+ /usr/local/cuda/include/vector_functions.hpp \
+ /usr/local/cuda/include/curand.h /usr/local/cuda/include/driver_types.h \
+ include/caffe/util/cudnn.hpp /usr/local/cuda/include/cudnn.h \
+ .build_release/src/caffe/proto/caffe.pb.h include/caffe/syncedmem.hpp
+
+include/caffe/util/hdf5.hpp:
+
+/home/dou/anaconda3/include/hdf5.h:
+
+/home/dou/anaconda3/include/H5public.h:
+
+/home/dou/anaconda3/include/H5pubconf.h:
+
+/home/dou/anaconda3/include/H5version.h:
+
+/home/dou/anaconda3/include/H5api_adpt.h:
+
+/home/dou/anaconda3/include/H5Apublic.h:
+
+/home/dou/anaconda3/include/H5Ipublic.h:
+
+/home/dou/anaconda3/include/H5Opublic.h:
+
+/home/dou/anaconda3/include/H5Lpublic.h:
+
+/home/dou/anaconda3/include/H5Tpublic.h:
+
+/home/dou/anaconda3/include/H5ACpublic.h:
+
+/home/dou/anaconda3/include/H5Cpublic.h:
+
+/home/dou/anaconda3/include/H5Dpublic.h:
+
+/home/dou/anaconda3/include/H5Epublic.h:
+
+/home/dou/anaconda3/include/H5Epubgen.h:
+
+/home/dou/anaconda3/include/H5Fpublic.h:
+
+/home/dou/anaconda3/include/H5FDpublic.h:
+
+/home/dou/anaconda3/include/H5Gpublic.h:
+
+/home/dou/anaconda3/include/H5MMpublic.h:
+
+/home/dou/anaconda3/include/H5Ppublic.h:
+
+/home/dou/anaconda3/include/H5Zpublic.h:
+
+/home/dou/anaconda3/include/H5PLpublic.h:
+
+/home/dou/anaconda3/include/H5Rpublic.h:
+
+/home/dou/anaconda3/include/H5Spublic.h:
+
+/home/dou/anaconda3/include/H5FDcore.h:
+
+/home/dou/anaconda3/include/H5FDfamily.h:
+
+/home/dou/anaconda3/include/H5FDlog.h:
+
+/home/dou/anaconda3/include/H5FDmpi.h:
+
+/home/dou/anaconda3/include/H5FDmpio.h:
+
+/home/dou/anaconda3/include/H5FDmulti.h:
+
+/home/dou/anaconda3/include/H5FDsec2.h:
+
+/home/dou/anaconda3/include/H5FDstdio.h:
+
+/home/dou/anaconda3/include/H5FDdirect.h:
+
+/home/dou/anaconda3/include/hdf5_hl.h:
+
+/home/dou/anaconda3/include/hdf5.h:
+
+/home/dou/anaconda3/include/H5DOpublic.h:
+
+/home/dou/anaconda3/include/H5DSpublic.h:
+
+/home/dou/anaconda3/include/H5LTpublic.h:
+
+/home/dou/anaconda3/include/H5IMpublic.h:
+
+/home/dou/anaconda3/include/H5TBpublic.h:
+
+/home/dou/anaconda3/include/H5PTpublic.h:
+
+include/caffe/blob.hpp:
+
+include/caffe/common.hpp:
+
+include/caffe/util/device_alternate.hpp:
+
+/usr/local/cuda/include/cublas_v2.h:
+
+/usr/local/cuda/include/cublas_api.h:
+
+/usr/local/cuda/include/driver_types.h:
+
+/usr/local/cuda/include/host_defines.h:
+
+/usr/local/cuda/include/cuComplex.h:
+
+/usr/local/cuda/include/vector_types.h:
+
+/usr/local/cuda/include/builtin_types.h:
+
+/usr/local/cuda/include/device_types.h:
+
+/usr/local/cuda/include/surface_types.h:
+
+/usr/local/cuda/include/texture_types.h:
+
+/usr/local/cuda/include/cuda_fp16.h:
+
+/usr/local/cuda/include/library_types.h:
+
+/usr/local/cuda/include/cuda.h:
+
+/usr/local/cuda/include/cuda_runtime.h:
+
+/usr/local/cuda/include/host_config.h:
+
+/usr/local/cuda/include/channel_descriptor.h:
+
+/usr/local/cuda/include/cuda_runtime_api.h:
+
+/usr/local/cuda/include/cuda_device_runtime_api.h:
+
+/usr/local/cuda/include/driver_functions.h:
+
+/usr/local/cuda/include/vector_functions.h:
+
+/usr/local/cuda/include/vector_functions.hpp:
+
+/usr/local/cuda/include/curand.h:
+
+/usr/local/cuda/include/driver_types.h:
+
+include/caffe/util/cudnn.hpp:
+
+/usr/local/cuda/include/cudnn.h:
+
+.build_release/src/caffe/proto/caffe.pb.h:
+
+include/caffe/syncedmem.hpp:
